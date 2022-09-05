ffce72802bd12c2b049656591c15318f2de51967
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-09-05 23:46:43 +0500

    Add getMiddle Node From LinkedList

--- Updated LinkedList/main.cpp
+++ Updated LinkedList/main.cpp
@@ -427,6 +427,23 @@ public:
             }
         }
     }
+    
+    Node* getMiddleNode(Node* head){
+        if(isEmpty()){ return NULL; }
+        else{
+            cout<<"Length of LinkedList "<<getLength()<<endl;
+            int middle = getLength() / 2;
+            // cout<<middle;
+            Node* temp = this->head;
+            for(int i = 0; i < middle; i++ ){
+                temp = temp->getNext();
+            }
+            return temp;
+        }
+    }
+    Node* getMiddleNode(){
+        return getMiddleNode(this->head);
+    }
 
 };
 
