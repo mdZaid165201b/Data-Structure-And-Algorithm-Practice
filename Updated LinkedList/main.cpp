43f5920655a43e386dc7f68e658e7cf3fadd8790
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-10-11 18:53:18 +0500

    Update main.cpp

--- Updated LinkedList/main.cpp
+++ Updated LinkedList/main.cpp
@@ -482,6 +482,27 @@ public:
             curr2->setNext(temp);
         }
     }
+    //without Sorting LinkedList removeDuplicates
+    void removeDuplicatesTwo(){
+        Node *temp = head;
+        Node *temp2;
+        while(temp != NULL){
+            Node *prev= temp;
+            temp2 = temp->getNext();
+            while(temp2 != NULL){
+                if(temp->getData() == temp2->getData()){
+                    Node *t = temp2->getNext();
+                    Node *t2 = temp2;
+                    delete(t2);
+                    temp2 = prev;
+                    prev->setNext(t);
+                }
+                prev = temp2;
+                temp2 = temp2->getNext();
+            }
+            temp = temp->getNext();
+        }
+    }
 
 };
 
