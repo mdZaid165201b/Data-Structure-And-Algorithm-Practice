b112b81142f11e30d326d9374e1788ebd984d5c7
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-10-13 02:32:16 +0500

    Update Selection Sort.cpp

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 void selectionSort(int arr[],int length){
     for(int i = 0; i < length-1; i++){
-        for(int  j = 0;j < length; j++){
-            if(arr[j] > arr[j+1]){
+        for(int  j = i+1;j < length; j++){
+            if(arr[i] > arr[j]){
                 int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[i];
+                arr[i] = temp;
             }
         }
     }
